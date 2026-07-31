aed9386ae31f5910579e51f5b64f63385cffd58d
ajsya <52182942+ajsya@users.noreply.github.com>
2026-07-31 13:58:36 -0400

    more abstraction + clean-up python code

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
+"""
+pomodoro-timer
+author: ajsya
+purpose: a pomodoro-timer for the BBC micro:bit
+usage:
+    - button a to start 25 min timer
+    - button b or shake to acknowledge when focus timer is off and start 5 min break timer
+    - button a + b to cancel timer
+"""
+
+# # input functions
+
 def on_button_pressed_a():
-    global index3
-    basic.show_string("Starting 25 Min Focus Session")
-    for index in range(25):
-        if canceled == False:
-            basic.show_number(25 - index)
-            basic.pause(60000)
-        else:
-            break
-    index3 = 25
-    while acknowledged == False:
-        doAnimation()
-    basic.show_string("Starting 5 Min Break")
-    for index2 in range(5):
-        basic.show_number(5 - index2)
-        basic.pause(60000)
-    basic.show_number(0)
-    basic.pause(5000)
+    global canceled
+    
+    doFocusTimer()
+
+    if canceled == False:
+        # do animation until timer is acknowledged
+        while acknowledged == False:
+            doAnimation()
+        
+        # do 5 min break timer
+        doBreakTimer()
+    
+    # reset pomodoro timer
     showPomodoro()
+    canceled = False
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
@@ -24,27 +33,38 @@ def on_button_pressed_ab():
     canceled = True
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
-def showPomodoro():
-    basic.show_leds("""
-        . . . . .
-        . # # # .
-        # # # # #
-        # # # # #
-        . # # # .
-        """)
-
 def on_button_pressed_b():
-    global acknowledged
-    if index32 == 25:
-        acknowledged = True
+    cancelTimer()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_gesture_shake():
-    global acknowledged
-    if index32 == 25:
-        acknowledged = True
+    cancelTimer()
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
+# # process functions
+def doFocusTimer():
+    global timerDone
+    for index in range(25):
+        if canceled == False:
+            basic.show_number(25 - index)
+            basic.pause(60000)
+        else:
+            break
+    timerDone = True
+
+def doBreakTimer():
+    for index2 in range(5):
+        basic.show_number(5 - index2)
+        basic.pause(60000)
+    basic.show_number(0)
+    basic.pause(5000)
+
+def cancelTimer():
+    global acknowledged
+    if timerDone == True:
+        acknowledged = True
+
+# # display functions
 def doAnimation():
     basic.show_leds("""
         . . . . .
@@ -95,13 +115,23 @@ def doAnimation():
         """)
     basic.pause(18)
 
-index32 = 0
-acknowledged = False
-index3 = 0
+def showPomodoro():
+    basic.show_leds("""
+        . . . . .
+        . # # # .
+        # # # # #
+        # # # # #
+        . # # # .
+        """)
+
+# started on boot
+
+## set variables to be used globally
 canceled = False
+acknowledged = False
+timerDone = False
+index32 = 0
+
+## do intro animation
 doAnimation()
 showPomodoro()
-
-def on_forever():
-    pass
-basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,50 +1,74 @@
-input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index <= 24; index++) {
+/** 
+pomodoro-timer
+author: ajsya
+purpose: a pomodoro-timer for the BBC micro:bit
+usage:
+    - button a to start 25 min timer
+    - button b or shake to acknowledge when focus timer is off and start 5 min break timer
+    - button a + b to cancel timer
+
+ */
+//  # input functions
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    doFocusTimer()
+    if (canceled == false) {
+        //  do animation until timer is acknowledged
+        while (acknowledged == false) {
+            doAnimation()
+        }
+        //  do 5 min break timer
+        doBreakTimer()
+    }
+    
+    //  reset pomodoro timer
+    showPomodoro()
+    canceled = false
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
+    canceled = true
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    cancelTimer()
+})
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    cancelTimer()
+})
+//  # process functions
+function doFocusTimer() {
+    
+    for (let index = 0; index < 25; index++) {
         if (canceled == false) {
             basic.showNumber(25 - index)
             basic.pause(60000)
         } else {
-            break;
+            break
         }
+        
     }
-    if (canceled == false) {
-        while (acknowledged == false) {
-            doAnimation()
-        }
-        basic.showString("Starting 5 Min Break")
-        for (let index2 = 0; index2 <= 4; index2++) {
-            basic.showNumber(5 - index2)
-            basic.pause(60000)
-        }
-        basic.showNumber(0)
-        basic.pause(5000)
-    }
-    showPomodoro()
-    canceled = false
-})
-input.onButtonPressed(Button.AB, function () {
-    canceled = true
-})
-function showPomodoro () {
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        # # # # #
-        # # # # #
-        . # # # .
-        `)
+    timerDone = true
 }
-input.onButtonPressed(Button.B, function () {
-    if (index32 == 25) {
+
+function doBreakTimer() {
+    for (let index2 = 0; index2 < 5; index2++) {
+        basic.showNumber(5 - index2)
+        basic.pause(60000)
+    }
+    basic.showNumber(0)
+    basic.pause(5000)
+}
+
+function cancelTimer() {
+    
+    if (timerDone == true) {
         acknowledged = true
     }
-})
-input.onGesture(Gesture.Shake, function () {
-    if (index32 == 25) {
-        acknowledged = true
-    }
-})
-function doAnimation () {
+    
+}
+
+//  # display functions
+function doAnimation() {
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -94,11 +118,23 @@ function doAnimation () {
         `)
     basic.pause(18)
 }
-let index32 = 0
-let acknowledged = false
+
+function showPomodoro() {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+}
+
+//  started on boot
+// # set variables to be used globally
 let canceled = false
+let acknowledged = false
+let timerDone = false
+let index32 = 0
+// # do intro animation
 doAnimation()
 showPomodoro()
-basic.forever(function () {
-	
-})
